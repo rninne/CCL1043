11f1769f4a672e38fad8f4267d8eab33ef6de94e
Robert Ninness <rninne@gmail.com>
2021-04-01 17:07:12 +1000

    Preparing for more actionHandlers

--- src/snc-now-experience-dashboard/snc-now-experience-dashboard.js
+++ src/snc-now-experience-dashboard/snc-now-experience-dashboard.js
@@ -1,10 +1,9 @@
-import { createHttpEffect } from '@servicenow/ui-effect-http';
-import { createCustomElement, actionTypes } from '@servicenow/ui-core';
-const { COMPONENT_BOOTSTRAPPED } = actionTypes;
+import { createCustomElement} from '@servicenow/ui-core';
+import { actionHandlers } from './actionHandlers';
 
 import styles from './styles.scss';
 
-import { columns, taskTables } from './defaults.js';
+import { columns } from './defaults.js';
 import '../components/now-experience-table';
 
 createCustomElement('snc-now-experience-dashboard', {
@@ -28,50 +27,7 @@ createCustomElement('snc-now-experience-dashboard', {
         );
     },
     actionHandlers: {
-        'FETCH_TASK_DATA': createHttpEffect('api/now/table/task', {
-            method: 'GET',
-            queryParams: [
-                'sysparm_fields',
-                'sysparm_query',
-                'sysparm_display_value',
-                'sysparm_exclude_reference_link'
-            ],
-            successActionType: 'FETCH_TASK_DATA_SUCCEEDED'
-        }),
-        [COMPONENT_BOOTSTRAPPED]: (coeffects) => {
-            const { dispatch } = coeffects;
-
-            const query = `sys_class_nameIN${taskTables.join(',')}`;
-
-            const fields = columns.map((col) => {
-                return col.field;
-            }).join(',');
-
-            dispatch('FETCH_TASK_DATA', {
-                sysparm_query: query,
-                sysparm_display_value: 'all',
-                sysparm_exclude_reference_link: true,
-                sysparm_fields: fields
-            });
-        },
-        'FETCH_TASK_DATA_SUCCEEDED': (coeffects) => {
-            const { action, updateState } = coeffects;
-            const { result } = action.payload;
-
-            const dataRows = result.map((row) => {
-                return Object.keys(row).reduce((acc, val) => {
-                    if (val === 'sys_class_name') {
-                        acc[val] = row[val].value;
-                    } else {
-                        acc[val] = row[val].display_value;
-                    }
-
-                    return acc;
-                }, {});
-            });
-
-            updateState({ dataRows });
-        }
+        ...actionHandlers
     },
     styles
 });
